13ca27909f119fb0635979771d037975f2ce2de3
moizkw <86135505+moizkw@users.noreply.github.com>
2021-11-08 19:34:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def main(url):
     soup = BeautifulSoup(xml_data, "lxml")
     child = soup.find("item")
     for i in range(3):
-        output = output + 'Article ' + str(i+1) + ' : ' +child.findChild("title").text + '. '
+        output = output + 'Article ' + str(i+1) + ': ' +child.findChild("title").text + '. \n'
         child = child.findNextSibling()
     return output
 
 def businessNews():
-    return(main("https://www.cnbc.com/id/10001147/device/rss/rss.html") + sponsor)
+    return(main("https://www.cnbc.com/id/10001147/device/rss/rss.html"))
     
 def worldNews():
     return(main("https://www.cnbc.com/id/100727362/device/rss/rss.html"))
@@ -21,7 +21,7 @@ def topNews():
     return(main("https://www.cnbc.com/id/100003114/device/rss/rss.html"))
 
 def earningsNews():
-    return(main("https://www.cnbc.com/id/15839135/device/rss/rss.html") + sponsor)
+    return(main("https://www.cnbc.com/id/15839135/device/rss/rss.html"))
 
 def realEstateNews():
     return(main("https://www.cnbc.com/id/10000115/device/rss/rss.html"))
@@ -36,25 +36,12 @@ def techNews():
     return(main("https://www.cnbc.com/id/19854910/device/rss/rss.html"))
 
 def financeNews():
-    return(main("https://www.cnbc.com/id/10000664/device/rss/rss.html") + sponsor)    
+    return(main("https://www.cnbc.com/id/10000664/device/rss/rss.html"))    
 
 def derivativesNews():
-    return(main("https://www.risk.net/feeds/rss/category/derivatives/equity-derivatives") + sponsor)    
-
-def esgNews():
-    output = ''
-    url = 'https://blogs.cfainstitute.org/investor/feed/'
-    xml_data = requests.get(url).content
-    soup = BeautifulSoup(xml_data, "lxml")
-    child = soup.find("channel")
-    for i in range(3):
-        output = output + 'Article ' + str(i+1) + ' : ' + child.findAll("title").text + '. '
-        child = child.findNextSibling()
-    return output 
-
+    return(main("https://www.risk.net/feeds/rss/category/derivatives/equity-derivatives"))    
 
 if __name__ == "__main__":
-    sponsor = "Pro-tip: Use the Wealthsimple Trade app to start online trading and get a free $25 bonus using our referral link: 'XXX'. \n"
     print(businessNews())
     print(worldNews())
     print(topNews()) 
@@ -65,4 +52,4 @@ if __name__ == "__main__":
     print(techNews()) 
     print(financeNews()) 
     print(derivativesNews()) 
-    print(esgNews()) 
+    
